8b052fd15893af9549a6373a0b30f3cfe6b2cca1
Adwaver4157 <adwaver4157@gmail.com>
2022-06-01 14:59:24 +0900

    feat: create maml encoder

---
+++ learning/MAML_feature_Extractor.py
@@ -0,0 +1,152 @@
+from torch.nn import (
+    Module,
+    Sequential,
+    Linear,
+    LeakyReLU,
+    Conv3d,
+    BatchNorm3d,
+    ConvTranspose3d,
+    BatchNorm1d,
+    Tanh
+)
+import torch
+import numpy as np
+from torch import split, exp, randn_like
+import torch.nn.functional as F
+from .utils import Flatten
+
+
+class MAML_Encoder(Module):
+    def __init__(self, embedding_dim, max_log_var=0.1):
+        super(MAML_Encoder, self).__init__()
+        self.embedding_dim = embedding_dim
+        self.max_log_var = max_log_var
+        self.emb_range_limit = Tanh()
+        self.net = Sequential(
+            Conv3d(1, 8, kernel_size=3, stride=1),
+            BatchNorm3d(8),
+            LeakyReLU(),
+            Conv3d(8, 16, kernel_size=3, stride=1),
+            BatchNorm3d(16),
+            LeakyReLU(),
+            Conv3d(16, 32, kernel_size=3, stride=1),
+            BatchNorm3d(32),
+            LeakyReLU(),
+            Conv3d(32, 64, kernel_size=3, stride=1),
+            BatchNorm3d(64),
+            LeakyReLU(),
+            Conv3d(64, 64, kernel_size=3, stride=2),
+            BatchNorm3d(64),
+            LeakyReLU(),
+            Conv3d(64, 32, kernel_size=3, stride=1),
+            BatchNorm3d(32),
+            LeakyReLU(),
+            Conv3d(32, 16, kernel_size=3, stride=1),
+            BatchNorm3d(16),
+            LeakyReLU(),
+            Conv3d(16, 8, kernel_size=3, stride=1),
+            BatchNorm3d(8),
+            LeakyReLU(),
+            Conv3d(8, 4, kernel_size=3, stride=1),
+            BatchNorm3d(4),
+            LeakyReLU(),
+            Flatten()
+        )
+
+    def forward(self, input):
+        output = self.net(input)
+        mu, logvar = split(output, self.embedding_dim, dim=1)
+
+        return mu, logvar * self.max_log_var
+
+    def reparameterize(self, input):
+        mu, logvar = self(input)
+        std = exp(logvar)
+        eps = randn_like(std)
+        output = self.emb_range_limit(mu + eps * std)
+        return output
+
+    def manual_forward(self, x, params):
+        
+        x = F.conv2d(x, params['conv1.weight'].to(device), params['conv1.bias'].to(device))
+        dumy = torch.ones(np.prod(np.array(x.data.size()[1]))).cuda()*999999999999999999 # momentnum=1
+        x = F.batch_norm(x, dumy, dumy, params['bn1.weight'], params['bn1.bias'], True, momentum=1)
+        x = F.max_pool2d(F.relu(x), 2)
+        
+        x = F.conv2d(x, params['conv2.weight'].to(device), params['conv2.bias'].to(device))
+        dumy = torch.ones(np.prod(np.array(x.data.size()[1]))).cuda()*999999999999999999 # momentnum=1
+        x = F.batch_norm(x, dumy, dumy, params['bn2.weight'], params['bn2.bias'], True, momentum=1)
+        x = F.max_pool2d(F.relu(x), 2)
+        
+        x = F.conv2d(x, params['conv3.weight'].to(device), params['conv3.bias'].to(device))
+        dumy = torch.ones(np.prod(np.array(x.data.size()[1]))).cuda()*999999999999999999 # momentnum=1
+        x = F.batch_norm(x, dumy, dumy, params['bn3.weight'], params['bn3.bias'], True, momentum=1)
+        x = F.max_pool2d(F.relu(x), 2)
+        
+        x = x.view(x.size(0), self.h)
+        x = F.linear(x, params['fc.weight'].to(device), params['fc.bias'].to(device))
+        x = F.log_softmax(x, dim=1)
+        
+        return x
+
+
+class MAML_Decoder(Module):
+    def __init__(self, embedding_dim):
+        super(MAML_Decoder, self).__init__()
+        self.embedding_dim = embedding_dim
+        self.mlp = Sequential(
+            Linear(self.embedding_dim, 1024),
+            BatchNorm1d(1024),
+            LeakyReLU(),
+            Linear(1024, 2048),
+            BatchNorm1d(2048),
+            LeakyReLU(),
+            Linear(2048, 4096),
+            BatchNorm1d(4096),
+            LeakyReLU()
+        )
+        self.deconv = Sequential(
+            ConvTranspose3d(64, 64, kernel_size=3, stride=1),
+            BatchNorm3d(64),
+            LeakyReLU(),
+            ConvTranspose3d(64, 64, kernel_size=3, stride=1),
+            BatchNorm3d(64),
+            LeakyReLU(),
+            ConvTranspose3d(64, 32, kernel_size=3, stride=3),
+            BatchNorm3d(32),
+            LeakyReLU(),
+            ConvTranspose3d(32, 16, kernel_size=3, stride=1),
+            BatchNorm3d(16),
+            LeakyReLU(),
+            ConvTranspose3d(16, 8, kernel_size=3, stride=1),
+            BatchNorm3d(8),
+            LeakyReLU(),
+            ConvTranspose3d(8, 4, kernel_size=3, stride=1),
+            BatchNorm3d(4),
+            LeakyReLU(),
+            ConvTranspose3d(4, 2, kernel_size=3, stride=1),
+            BatchNorm3d(2),
+            LeakyReLU(),
+            ConvTranspose3d(2, 1, kernel_size=3, stride=1),
+            BatchNorm3d(1),
+            LeakyReLU(),
+            ConvTranspose3d(1, 1, kernel_size=3, stride=1),
+            BatchNorm3d(1),
+            LeakyReLU(),
+            ConvTranspose3d(1, 1, kernel_size=3, stride=1),
+            BatchNorm3d(1),
+            LeakyReLU(),
+            ConvTranspose3d(1, 1, kernel_size=3, stride=1),
+            BatchNorm3d(1),
+            Tanh()
+        )
+
+    def forward(self, input):
+        output = self.mlp(input)
+        output = output.view(input.size()[0], 64, 4, 4, 4)
+        return self.deconv(output)
+
+if __name__=="__main__":
+    embedding_dim = 686
+    encoder = MAML_Encoder(686)
+    decoder = MAML_Decoder(686)
